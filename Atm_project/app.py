6e4b5de2812c1b74476d1a257cf07222f2d36370
n2van <106149287+n2van@users.noreply.github.com>
2025-05-15 11:01:34 +0700

    Update app.py

--- Atm_project/app.py
+++ Atm_project/app.py
@@ -770,7 +770,7 @@ with gr.Blocks(theme=theme, css=custom_css, title="<MongolianWigs - Try-on Wigs"
             
         wig_gallery.select(
             fn=select_wig,
-            inputs=[select_wig],
+            inputs=[wig_gallery],
             outputs=[image_input]
         )
         
